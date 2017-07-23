e52d3f4ce9a462076c2442c5df65f6290318aa26
dileepgm <getdileepgm@gmail.com>
2017-07-23 18:21:13 +0100

    set interval ping

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-    const express = require('express');
-    const cors = require('cors');
-    const searchFacade = require('./lib/search-facade');
+const express = require('express');
+const cors = require('cors');
+const searchFacade = require('./lib/search-facade');
 
 
 
-    const app = express();
-    app.use(cors());
+const app = express();
+app.use(cors());
 
-    app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-    app.get('/', function (request, response) {
-      response.send('Hello QUAENT!')
-    });
+app.get('/', function (request, response) {
+  response.send('Hello QUAENT!')
+});
 
-    app.get('/search/:searchText', function (req, res) {
-      //const searchText = 'What are the upcoming trends on chatbots?';
-      const searchText = req.params['searchText'];
-      console.log(searchText);
-      searchFacade.search(searchText).then((data)=>{
-        console.log('Success...');
-         res.json(data);
-      });
-      // Hard coding for simplicity. Pretend this hits a real database
+app.get('/search/:searchText', function (req, res) {
+  //const searchText = 'What are the upcoming trends on chatbots?';
+  const searchText = req.params['searchText'];
+  console.log(searchText);
+  searchFacade.search(searchText).then((data) => {
+    console.log('Success...');
+    res.json(data);
+  });
+  // Hard coding for simplicity. Pretend this hits a real database
 
-    });
+});
 
-    app.listen(app.get('port'), function () {
-      console.log("Node app is running at localhost:" + app.get('port'));
-    });
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'));
+  var http = require("http");
+  setInterval(function () {
+    http.get("https://hidden-spire-31316.herokuapp.com");
+  }, 300000);
+});
 
 
 
